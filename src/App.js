574b7a42fef9f482ee6c52b52b0580186508873c
Bivek <bivek.tamu@gmail.com>
2023-10-16 17:05:24 +1100

    Library demo inititated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {Ticker} from 'react-ticker-tape'
+import './App.css'
 function App() {
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Ticker Tape Library Demo
       </header>
+      <main>
+          <Ticker text= {"Hi there! I am ticker tape soley build for react projects."} animationSpeed = {20} bg= {'red'} color={'white'}  />
+          <Ticker text='Hi there! I am another ticker tape. Look I can be placed in bottom.' animationSpeed = {20} bg= {'yellow'} color={'black'} fix={"bottom"} />
+          <Ticker text='Hi there! I am third ticker tape placed on left side.' animationSpeed = {15} bg= {'lightgreen'} color={'red'} fix={"left"} />
+          <Ticker text='Hi there! I am fourth and last ticker tape placed on right side.' animationSpeed = {25} bg= {'blue'} color={'white'} fix={"right"} />
+      </main>
     </div>
   );
 }
